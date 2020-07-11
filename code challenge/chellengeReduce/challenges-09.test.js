c9c34cd503ceb817549c3066ee39f41da2450813
abdulelahxd <abdulelahkassara@gmail.com>
2020-07-11 20:07:13 +0300

    newChallenges

--- code challenge/chellengeReduce/challenges-09.test.js
+++ code challenge/chellengeReduce/challenges-09.test.js
@@ -281,6 +281,7 @@ Note: You must use reduce for this challenge. You may not use the built-in .reve
 
 const reversedString = (str) => {
   // Solution code here...
+  // return str.
 };
 
 /* ------------------------------------------------------------------------------------------------
